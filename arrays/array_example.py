aae14e427e804b6944427a324796b8eb3b518952
absunandan008 <absunandan008@gmail.com>
2024-06-16 13:43:07 -0400

    array example with methods

--- arrays/array_example.py
+++ arrays/array_example.py
@@ -1,31 +1,71 @@
 # import array
 import array
 
-# initialize array with some values, i is the data type of array
+# INITIALIZE array with some values, i is the data type of array
 arr = array.array('i', [1,2,3])
 
 # printing original array, the end argument prevents from adding a new line after each print
-print("the new created array is : ", end=" ")
+print("INITIALIZE: the new created array is : ", end=" ")
 for i in range(0,3):
     print(arr[i], end=" ")
 print("\r")
-print("--")
+print("------ End-")
 
-#add an element at end of array
+#APPEND: add an element at end of array
 arr.append(4)
 
-print("Printing array after appending 4th element", end=" ")
+print("APPEND: Printing array after appending 4th element", end=" ")
 for i in range(len(arr)):
     print(arr[i], end=" ")
 print("\r")
-print("--")
+print("------ APPEND End-")
 
-#insert element at 3rd position
+#INSERT: insert an element at 3rd position and it increases the array size O(n) worst time
+# and space complexity is O(n)
 arr.insert(2,5)
 
 #print array again
-print("new array after inserting is: ", end=" ")
+print("INSERT: new array after inserting is: ", end=" ")
 for i in range(len(arr)):
     print(arr[i], end=" ")
 print("\r")
-print("--")
+print("------ INSERT End-")
+
+#POP: remoce an element, it removes the said element from the given position
+arr.pop(2)
+print("POP: Printing array after pop : ", end=" ")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
+print("\r")
+print("------ POP End-")
+
+#REMOVE: array remove method, this is used to remove first occurence of the value
+arr.remove(3)
+print("REMOVE: Printing array after remove : ", end=" ")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
+print("\r")
+print("------ REMOVE End-")
+
+#adding extra members
+arr.append(1)
+arr.append(2)
+arr.append(5)
+
+#INDEX: array index method returns first occurence of the value
+print("INDEX: Printing array after append : ", end=" ")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
+print("\r")
+print("print first occurence of 2 :", end=" ")
+print(arr.index(2), end=" ")
+print("\r")
+print("------ INDEX End-")
+
+#REVERSE: this reverses the array
+arr.reverse()
+print("REVERSE: Printing array after reverse : ", end=" ")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
+print("\r")
+print("------ REVERSE End-")
