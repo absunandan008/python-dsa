939256e2afd3406ff0efe5b614cd75ddc5d9b5d7
absunandan008 <absunandan008@gmail.com>
2024-06-21 11:53:55 -0400

    adding 2 sum sequence

--- arrays/twosum.py
+++ arrays/twosum.py
@@ -4,11 +4,11 @@ class Solution:
         mem = {}
         for i in range(len(nums)):
             if target - nums[i] in mem:
-                return [i, mem[target - nums[i]]]
+                return [mem[target-nums[i]], i]
             else:
                 mem[nums[i]] = i
 
 instance = Solution()
 print(instance.twoSum([2,7,11,15], 9) , "|Output: [0,1]")
 print(instance.twoSum([3,2,4], 6) , "|Output: [1,2]")
-print(instance.twoSum([3,3], 6) , "|Output: [1,2]")
+print(instance.twoSum([3,3], 6) , "|Output: [0,1]")
