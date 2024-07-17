77b7262a6309befd715252286628328357c9532f
absunandan008 <absunandan008@gmail.com>
2024-07-17 19:43:49 -0400

    add invert binary tree

--- trees/problems/delete_node_binary_tree_450.py
+++ trees/problems/delete_node_binary_tree_450.py
@@ -3,12 +3,41 @@ from typing import Optional
 
 
 class TreeNode:
+
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+    def insert(self, value):
+        if self is None:
+            self.val = value
+        else:
+            self._recursive_insert(self, value)
+
+    def _recursive_insert(self, node, value):
+        if node.val < value:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._recursive_insert(node.right, value)
+        elif node.val > value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._recursive_insert(node.left, value)
+
+    def print_tree(self):
+        def _print_recursive(node, level=0, prefix="Root:"):
+            if node is not None:
+                print(" " * (level * 4) + prefix + str(node.val))
+                _print_recursive(node.left, level + 1, "L---")
+                _print_recursive(node.right, level + 1, "R---")
+
+        _print_recursive(self)
+
+
+    def deleteNode(self, root, key: int):
         if root is None:
             return None
 
@@ -30,3 +59,21 @@ class Solution:
         return root
 
 
+
+
+
+# Example usage
+tree = TreeNode()
+
+# Insert nodes
+nodes_to_insert = [5, 3, 7, 2, 4, 6, 8]
+for node in nodes_to_insert:
+    tree.insert(node)
+
+print("Initial tree:")
+tree.print_tree()
+
+tree.deleteNode(tree, 3)
+
+print("Deleted  tree:")
+tree.print_tree()
---
+++ trees/problems/invert_binary_tree_226.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def insert(self, value):
+        if self is None:
+            self,val = value
+        else:
+            self._recursive_insert(self,value)
+
+    def print_tree(self):
+        def _print_recursive(node, level=0, prefix="Root:"):
+            if node is not None:
+                print(" " * (level * 4) + prefix + str(node.val))
+                _print_recursive(node.left, level + 1, "L---")
+                _print_recursive(node.right, level + 1, "R---")
+
+        _print_recursive(self)
+
+    def _recursive_insert(self,node,value):
+        if value < node.val:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._recursive_insert(node.left,value)
+        elif value > node.val:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._recursive_insert(node.right,value)
+    def invertTree(self, root):
+        #NonOptimal solution. Go through tree and then swap the left and right nodes
+        if root is None:
+            return root
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        #Now we exchange the nodes
+        dummy = root.left
+        root.left = root.right
+        root.right = dummy
+
+        return root
+
+
+
+# Example usage
+tree = TreeNode()
+
+# Insert nodes
+nodes_to_insert = [5, 3, 7, 2, 4, 6, 8]
+for node in nodes_to_insert:
+    tree.insert(node)
+
+print("Initial tree:")
+tree.print_tree()
+
+tree.invertTree(tree)
+
+print("Inverted  tree:")
+tree.print_tree()
