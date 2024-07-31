2c539c746714fd56b3239bf9909d280c748a7b3d
absunandan008 <absunandan008@gmail.com>
2024-07-30 22:04:57 -0400

    add another version pow of x,n 50

--- mtag_problems/power_x_n_50.py
+++ mtag_problems/power_x_n_50.py
@@ -13,3 +13,14 @@ class Solution:
             x *= x
             n //= 2
         return result
+
+    def myPow2(self, x: float, n: int) -> float:
+        def helper(x,n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            res = helper(x*x, n//2)
+            return res*x if n % 2 == 1 else res
+        res = helper(x,abs(n))
+        return 1/res if n < 0 else res
