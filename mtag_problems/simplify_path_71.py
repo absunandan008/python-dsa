4e5b2597b1cd12d5fb2bfd2c47a8fa1ffdad0d5e
absunandan008 <absunandan008@gmail.com>
2024-08-07 11:56:00 -0400

    add Merge Two Sorted Lists 21 and simplify_path_71

--- mtag_problems/simplify_path_71.py
+++ mtag_problems/simplify_path_71.py
@@ -10,4 +10,28 @@ class Solution:
             else:
                 stack.append(portion)
         final_str = "/" + "/".join(stack)
+        '''
+        simplified_path = '/'
+        for i, dir in enumerate(stack):
+            simplified_path += dir
+            if i < len(stack) - 1:
+                simplified_path += '/'
+        return simplified_path
+        '''
+
         return final_str
+
+# Test cases
+test_cases = [
+    "/home/",
+    "/home//foo/",
+    "/home/user/Documents/../Pictures",
+    "/../",
+    "/.../a/../b/c/../d/./"
+]
+
+solution = Solution()
+for case in test_cases:
+    print(f"Input: {case}")
+    print(f"Output: {solution.simplifyPath(case)}")
+    print()
