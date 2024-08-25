af27f0c0d8e18087acd6859deaa2d78ca07b4c24
absunandan008 <absunandan008@gmail.com>
2024-08-25 12:36:17 -0400

    adding better time complexity top_k_frequent_elements_347

--- mtag_problems/top_k_frequent_elements_347.py
+++ mtag_problems/top_k_frequent_elements_347.py
@@ -7,14 +7,31 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        frequencyData = {}
-        for n in nums:
-            frequencyData[n] = 1 + frequencyData.get(n, 0)
-        heap =  []
-        for key,value in frequencyData.items():
-            if len(heap) < k:
-                heapq.heappush(heap, (value,key))
-            else:
-                heapq.heappushpop(heap, (value,key))
+        # O(N)
+        frequcy_map = defaultdict()
+        bucket_array = [[] for i in range(len(nums) + 1)]
+        result = []
+        for i in range(len(nums)):
+            frequcy_map[nums[i]] = 1 + frequcy_map.get(nums[i], 0)
+        for num, val in frequcy_map.items():
+            bucket_array[val].append(num)
+        for i in range(len(bucket_array) - 1, 0, -1):
+            if bucket_array[i] != []:
+                for j in bucket_array[i]:
+                    result.append(j)
+                    k -= 1
+            if k == 0:
+                break
+        return result
 
-        return [h[1] for h in heap]
+        """
+        #O(n log k)
+        freqncy_map = collections.Counter(nums)
+        heap = []
+        for num,freq in freqncy_map.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (freq,num))
+            else:
+                heapq.heappushpop(heap, (freq,num))
+        return [x[1] for x in heap]
+        """
