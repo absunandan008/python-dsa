1c1c630c66f5ab880d520dc0ee2f0ab0db0544e8
absunandan008 <absunandan008@gmail.com>
2024-10-22 08:31:59 -0400

    add 3 sum closet

--- mtag_problems/3_sum_closest_16.py
+++ mtag_problems/3_sum_closest_16.py
@@ -14,3 +14,33 @@ Input: nums = [0,0,0], target = 1
 Output: 0
 Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
 """
+
+import math
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = math.inf
+        n = len(nums)
+        
+        nums.sort()
+        
+        for i in range(n-2):
+            lo = i+1
+            hi = n-1
+            while lo < hi:
+                sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target-sum) < abs(diff):
+                    diff = target-sum
+                if sum > target:
+                    hi -= 1
+                else:
+                    lo += 1
+                    
+            if diff == 0:
+                break
+        
+        return target-diff
+        #O(n^2)
+        #O(1)
