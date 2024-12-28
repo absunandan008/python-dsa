fcd869c6f3e726670e3174b3ff14d51329448913
Abhishek Sunandan <abhisun@macbook-pro.myfiosgateway.com>
2024-12-28 14:04:55 -0500

    changes to 2 sum, staring prep again

--- arrays/problems/twosum.py
+++ arrays/problems/twosum.py
@@ -2,11 +2,11 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mem = {}
-        for i in range(len(nums)):
-            if target - nums[i] in mem:
-                return [mem[target-nums[i]], i]
-            else:
-                mem[nums[i]] = i
+        for i,num in enumerate(nums):
+            if target-num in mem:
+                return [mem[target-num], i]
+            mem[num] = i
+        return []
 
 instance = Solution()
 print(instance.twoSum([2,7,11,15], 9) , "|Output: [0,1]")
