2e5e345b6d972f0fec88a16fc903e221d7fe0fd3
Abhishek Sunandan <absunandan008@gmail.com>
2024-12-28 14:07:16 -0500

    verified commit if possible

--- arrays/problems/twosum.py
+++ arrays/problems/twosum.py
@@ -2,6 +2,7 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mem = {}
+        #Enumerate gives indice and value both
         for i,num in enumerate(nums):
             if target-num in mem:
                 return [mem[target-num], i]
